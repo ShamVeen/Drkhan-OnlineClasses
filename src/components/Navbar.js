bdd386478671ae2d74f22c6b5c022bcbfb8473f9
ShamVeen <118723873+ShamVeen@users.noreply.github.com>
2023-05-02 19:02:24 +0530

    code updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,4 @@
-// import React, { useState } from 'react'
-// import { FaBars, FaTimes } from 'react-icons/fa'
-// import logo from '../images/Dr. Khan LOGO.svg';
-
-// // import logo from '../images/menu.png';
-// import '../Css/Navbar.css';
-
-// const Navbar = () => {
-
-//     const [click, setClick] = useState(false)
-//     const handleClick = () => setClick(!click)
-
-//     const closeMenu = () => setClick(false)
-
-//     return (
-//         <div className='header'>
-//             <nav className='navbar'>
-//                 <a href='/' className='logo'>
-//                     <img src={logo} alt='logo' />
-//                 </a>
-//                 <div className='hamburger' onClick={handleClick}>
-//                     {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
-//                         : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
-
-//                 </div>
-//                 <ul className={click ? "nav-menu active" : "nav-menu"}>
-//                     <li className='nav-item'>
-//                         <a href='/' onClick={closeMenu}>Home</a>
-//                     </li>
-//                     <li className='nav-item'>
-//                         <a href='#about' onClick={closeMenu}>About Us</a>
-//                     </li>
-//                     <li className='nav-item'>
-//                         <a href='#testimonials' onClick={closeMenu}>Testimonials</a>
-//                     </li>
-//                     <li className='nav-item'>
-//                         <a href='#faq' onClick={closeMenu}>FAQ</a>
-//                     </li>
-//                     <li className='nav-item'>
-//                         <a href='#neet' onClick={closeMenu}>NEET</a>
-//                     </li>
-
-//                     <li className='nav-item'>
-//                         <a href='#callus' onClick={closeMenu}>ContactUs</a>
-//                     </li>
-//                 </ul>
-//             </nav>
-//         </div>
-//     )
-// }
-
-// export default Navbar
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Css/Navbar.css';
 import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
@@ -64,6 +11,7 @@ const Navbar = () => {
   const handleClick = () => setClick(!click);
 
   const closeMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
@@ -110,10 +58,6 @@ const Navbar = () => {
           </li>
         </ul>
 
-        {/* </ul> */}
-        {/* 
-        whenever we click on buthrefn = setMobile(!Mobile) ==  is mobile oppsite to setMobile 
-        */}
         <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>
           {Mobile ? <ImCross /> : <FaBars />}
         </button>
